4ef7592da69cd45310ceccb9e84968832ace533c
Vyduikin <i.vyduikin@mkskom.ru>
2022-07-22 01:32:16 +0300

    feat(dto): Добавить dto book

---
+++ src/books/dto/index.js
@@ -0,0 +1,12 @@
+const book = {
+  id: '',
+  title: '',
+  description: '',
+  authors: '',
+  favorite: false,
+  fileCover: '',
+  fileName: '',
+  fileBook: '',
+};
+
+module.exports = { book };
