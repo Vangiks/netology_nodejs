acba7d166e0ce76e6d548f39f70cd9940f3ce3f7
Vyduikin <i.vyduikin@mkskom.ru>
2022-08-24 01:26:14 +0300

    feat: Реализовать чат на странице просмотра книги

--- main.js
+++ main.js
@@ -7,6 +7,9 @@ const mongoose = require('mongoose');
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 
+const http = require('http');
+const socketIO = require('socket.io');
+
 const Users = require('./src/users/users.service');
 
 passport.use('local', new LocalStrategy(Users.options, Users.verify));
@@ -26,6 +29,9 @@ passport.deserializeUser(async (id, cb) => {
 const express = require('express'),
   app = express();
 
+const server = http.Server(app);
+const io = socketIO(server);
+
 const host = config.APP_BASE_PATH;
 const port = config.PORT;
 const databasePath = config.DATABASE_PATH;
@@ -34,6 +40,10 @@ console.log(config);
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/src/views');
+
+app.get('/', (req, res, next) => {
+  res.render('index');
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -51,9 +61,27 @@ async function main() {
   await mongoose.connect(databasePath);
 }
 
+io.on('connection', (socket) => {
+  const { id } = socket;
+  console.log(`Socket connected: ${id}`);
+
+  const { roomName } = socket.handshake.query;
+  
+  socket.join(roomName);
+  socket.on('message-to-room', (msg) => {
+    msg.type = `room: ${roomName}`;
+    socket.to(roomName).emit('message-to-room', msg);
+    socket.emit('message-to-room', msg);
+  });
+
+  socket.on('disconnect', () => {
+    console.log(`Socket disconnected: ${id}`);
+  });
+});
+
 main()
   .then(() => {
-    app.listen(port, () => {
+    server.listen(port, () => {
       console.log(`Server listens http://${host}:${port}`);
     });
   })
